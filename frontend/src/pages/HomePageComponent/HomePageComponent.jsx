bfaf0022a10f8f84c5701464a66ca90ac3f5f9bb
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2025-07-01 08:03:38 +0200

    homepage in progress

--- frontend/src/pages/HomePageComponent/HomePageComponent.jsx
+++ frontend/src/pages/HomePageComponent/HomePageComponent.jsx
@@ -1,75 +1,68 @@
 import "./HomePageComponent.css"; // if you have styling
 
 const HomePageComponent = () => {
-  return (
-    <div className="ComponentContainer">
-    <div
-    className="HomePageBackgroundImgContainer"
-      style={{
-        width: '100%',
-        height: 'auto',
-        backgroundImage: `url('/img/handwriting.jpg')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        // opacity: '0.7',
-        padding: '20px'
-      }}
-    >
-           <div className="HomePageWelcomeText">
-        <h2>Välkommen till</h2>      
-        <h1>WEBBLOGOPEDEN</h1>  
-        <h3>Din logoped i hela Sverige</h3>
-        <h4>Dyslexiutredningar och språkliga utredningar online i privat regi</h4>
-        <h4>Korta väntetider och en personlig vårdupplevelse</h4>
-      </div>  
-  
-</div>
-<div className="HomePageServices">
-  <div className="DyslexiaImgContainer">
-    <img
-                src="/img/personsittingatcomputer.jpg"
-                alt="Site Logo"
-                className="logo"
-            />
-  </div>
-  <div className="DyslexiaPrice">
-  <h2 className="DyslexiaTitle">Dyslexiutredning</h2>
-  
-  </div>
-  <div className="m10px">
-  <p>3-6 timmar</p>
-  <p className="m10px">8 000 kr</p>
-  <p>För alla åldrar</p>
-  <p>Väntetid 1-2 veckor</p>
-  <p>Ingår omfattande utredning samt intyg</p>
-  </div>
-  <button className="m30px">Boka nu</button>
+return (
+<div className="ComponentContainer">
 
-  <div className="DyslexiaImgContainer">
-    <img
-                src="/img/personsittingatcomputer.jpg"
-                alt="Site Logo"
-                className="logo"
-            />
+  <div className="HomePageBackgroundImgContainer" style={{ backgroundImage: `url("/img/notebook.jpg")` }}>
+    <div className="HomePageWelcomeText">
+      <h2>Välkommen till</h2>
+      <h1>WEBBLOGOPEDEN</h1>
+      <h3>Din logoped i hela Sverige</h3>
+      <h4>Dyslexiutredningar och språkliga utredningar online i privat regi</h4>
+      <h4>Korta väntetider och inget krav på remiss</h4>
+    </div>
+    <div className="homePageContactButton">
+      <button>Kontakta mig idag för mer info och tidsbokning</button>
+    </div>
   </div>
-  <div className="DyslexiaPrice">
-  <h2 className="DyslexiaTitle">Språkutredning</h2>
-  
-  </div>
-  <div className="m10px">
-  <p>3-6 timmar</p>
-  <p className="m10px">8 000 kr</p>
-  <p>För alla åldrar</p>
-  <p>Väntetid 1-2 veckor</p>
-  <p>Ingår omfattande utredning samt intyg</p>
-  </div>
-  <button>Boka nu</button>
+
+  {/* <div className="HomePageWelcomeText">
+    <h2>Välkommen till</h2>
+    <h1>WEBBLOGOPEDEN</h1>
+    <h3>Din logoped i hela Sverige</h3>
+    <h4>Dyslexiutredningar och språkliga utredningar online i privat regi</h4>
+    <h4>Korta väntetider och inget krav på remiss</h4>
+  </div> */}
+
+  <div className="HomePageServices">
+    <div className="DyslexiaImgContainer">
+      <img src="/img/books.jpg" alt="Site Logo" className="logo" />
+    </div>
+    <div className="DyslexiaPrice">
+      <h2 className="DyslexiaTitle">Dyslexiutredning</h2>
+
+    </div>
+    <div className="m10px">
+      <p>3-6 timmar</p>
+      <p className="m10px">8 000 kr</p>
+      <p>För alla åldrar</p>
+      <p>Väntetid 1-2 veckor</p>
+      <p>Ingår omfattande utredning samt intyg</p>
+    </div>
+    <button className="m30px">Kontakt och mer info</button>
+
+    <div className="DyslexiaImgContainer">
+      <img src="/img/personsittingatcomputer.jpg" alt="Site Logo" className="logo" />
+    </div>
+    <div className="DyslexiaPrice">
+      <h2 className="DyslexiaTitle">Språkutredning</h2>
+
+    </div>
+    <div className="m10px">
+      <p>3-6 timmar</p>
+      <p className="m10px">8 000 kr</p>
+      <p>För alla åldrar</p>
+      <p>Väntetid 1-2 veckor</p>
+      <p>Ingår omfattande utredning samt intyg</p>
+    </div>
+    <button>Kontakt och mer info</button>
 
 
-</div>
+  </div>
 </div>
 
-  );
+);
 };
 
 export default HomePageComponent;
