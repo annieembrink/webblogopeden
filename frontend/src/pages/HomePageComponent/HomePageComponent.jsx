72821bdb2c8a06cf423b93e4b5b6b2b544b0041a
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2025-07-01 08:21:02 +0200

    changed img for language investigation

--- frontend/src/pages/HomePageComponent/HomePageComponent.jsx
+++ frontend/src/pages/HomePageComponent/HomePageComponent.jsx
@@ -33,30 +33,30 @@ return (
       <h2 className="DyslexiaTitle">Dyslexiutredning</h2>
 
     </div>
-    <div className="m10px">
+    <div className="m30px">
       <p>3-6 timmar</p>
       <p className="m10px">8 000 kr</p>
       <p>För alla åldrar</p>
       <p>Väntetid 1-2 veckor</p>
       <p>Ingår omfattande utredning samt intyg</p>
     </div>
-    <button className="m30px">Kontakt och mer info</button>
+    <button className="m60px">Kontakt och mer info</button>
 
     <div className="DyslexiaImgContainer">
-      <img src="/img/personsittingatcomputer.jpg" alt="Site Logo" className="logo" />
+      <img src="/img/holdinghands.jpg" alt="Site Logo" className="logo" />
     </div>
     <div className="DyslexiaPrice">
       <h2 className="DyslexiaTitle">Språkutredning</h2>
 
     </div>
-    <div className="m10px">
+    <div className="m30px">
       <p>3-6 timmar</p>
       <p className="m10px">8 000 kr</p>
       <p>För alla åldrar</p>
       <p>Väntetid 1-2 veckor</p>
       <p>Ingår omfattande utredning samt intyg</p>
     </div>
-    <button>Kontakt och mer info</button>
+    <button className="m60px">Kontakt och mer info</button>
 
 
   </div>
