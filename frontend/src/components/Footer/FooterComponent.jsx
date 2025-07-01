261cac24caeee70d7ea59219f53f18a6c04c4939
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2025-07-01 08:48:33 +0200

    footer logo and contact

--- frontend/src/components/Footer/FooterComponent.jsx
+++ frontend/src/components/Footer/FooterComponent.jsx
@@ -1,7 +1,22 @@
 import "./FooterComponent.css"; // if you have styling
+import { FaRegCopyright } from "react-icons/fa";
+
 
 const FooterComponent = () => {
-    return ( <div className="Footer"></div> );
+    return ( <div className="Footer">
+         <div id="footerImgContainer">
+            <img
+                src="/img/webblogopeden-high-resolution-logo-transparent.png"
+                alt="Site Logo"
+                className="logo"
+            />
+        </div>
+        <div id="footerContact">
+            <p>Farsta, Stockholm</p>
+            <p>webblogopeden@gmail.com</p>
+            <p><FaRegCopyright style={{ marginRight: "5px" }} />Copyright 2025, webblogopeden</p>
+        </div>
+    </div> );
 }
  
 export default FooterComponent;
